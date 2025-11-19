3a92d6d0040d4b39ef5bc434a018fe308ff21d95
Riya Singh <riya160305@gmail.com>
2025-11-19 12:21:05 +0530

    update

--- script.js
+++ script.js
@@ -142,3 +142,15 @@ window.addEventListener('load', () => {
 });
 
 console.log('🚀 ACM Website loaded successfully!');
+const btn = document.getElementById("viewMoreBtn");
+const more = document.getElementById("moreMembers");
+
+btn.addEventListener("click", () => {
+    more.classList.toggle("hidden");
+
+    if (more.classList.contains("hidden")) {
+        btn.textContent = "View More";
+    } else {
+        btn.textContent = "View Less";
+    }
+});
